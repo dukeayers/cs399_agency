6e76c03bdaa6355b4a71d9ba15cdd2404d3de89d
Duke Ayers <Dfa24@nau.edu>
2015-02-15 21:37:12 -0700

    Added Campaigns
    Added the campaign pages and made a request to get the information of
    each campaign

--- agency/agency/urls.py
+++ agency/agency/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import home, about, lists
+from views import home, about, location, campaign, lists
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'agency.views.home', name='home'),
@@ -8,5 +8,9 @@ urlpatterns = patterns('',
     url(r'^$', home),
     url(r'^about', about),
     url(r'^lists', lists),
+    url(r'^$', home),
+    url(r'^about', about),
+    url(r'^location', location),
+    url(r'^campaign/(?P<question_id>\d+)/$', campaign, name="campaign"),
     # url(r'^admin/', include(admin.site.urls)),
 )
--- agency/views.py
+++ agency/views.py
@@ -1,13 +1,39 @@
-from django.shortcuts import render
 from django.core import serializers
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from agency.models import LocationForm, Campaign
 # Create your views here.
+
+
+def lists(request):
+    return render(request, 'lists.html')
 
 def home(request):
     return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {'campaigns': Campaign.objects.all()})
 
-def lists(request):
-    return render(request, 'lists.html')
+def campaign(request, question_id):
+    if request.method == 'POST':
+        form = LocationForm(request.POST)
+
+        if(form.is_valid()):
+            form.save(commit = True)
+            return HttpResponseRedirect('/')
+    else:
+        form = LocationForm()
+    return render(request, 'campaign.html', {'CurrentCampaign': Campaign.objects.filter(id = question_id), 'form': form})
+    #return HttpResponse("You want to see %s." %question_id)
+
+def location(request):
+    if request.method == 'POST':
+        form = LocationForm(request.POST)
+
+        if(form.is_valid()):
+            form.save(commit = True)
+            return HttpResponseRedirect('/')
+    else:
+        form = LocationForm()
+
+    return render(request, 'about.html', {'form': form})
