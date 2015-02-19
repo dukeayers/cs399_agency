6cee56f91bd0041910c08dc2165e74955611ecd3
cmh553 <cmh553@nau.edu>
2015-02-18 20:09:24 -0700

    New campaigns
    Adding two new campaigns to migrations

--- agency/agency/migrations/0003_auto_20150219_0203.py
+++ agency/agency/migrations/0003_auto_20150219_0203.py
@@ -6,10 +6,10 @@ import datetime
 def add_data(apps, schema_editor):
     Campaign = apps.get_model("agency", "Campaign")
 
-    CampaignTitles = ["Send-A-Cookie", "Glitter Bombs!"]
+    CampaignTitles = ["Send-A-Cookie", "Glitter Bombs!", 'Adopt-a-Gerbil', 'Pop-up Carnival']
     CampaignDescription = ["Send a Cookie to a friend by filling out the form in regards to their location. Once completed, your friend will get an Oatmeal Raisin cookie labeled a Chocolate Chip! Way to be tricky.", "Ever wanted to get back at someone but just did not know how?  Send them a glitter bomb!  A tube of glitter will be sent to an address of your choosing and when it is opened someone will be in for a surprise!"]
-    CampaignBegin = ["2015-02-15", "2015-02-15"]
-    CampaignEnd = ["2015-05-23", "2015-08-30"]
+    CampaignBegin = ["2015-02-15", "2015-02-15","2015-09-12", "2015-12-01"]
+    CampaignEnd = ["2015-05-23", "2015-08-30","2015-12-15", "2015-02-20"]
 
     for i in range(0,2):
         data = Campaign(title = CampaignTitles[i], description = CampaignDescription[i], BeginDate = CampaignBegin[i], EndDate = CampaignEnd[i])
